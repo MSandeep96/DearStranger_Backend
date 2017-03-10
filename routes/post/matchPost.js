0def25881567231423406dfc61d7659311f1c437
Sandeep Reddy <msande1996@gmail.com>
2017-03-10 21:48:40 +0530

    Regulate matching to 7 days

--- routes/post/matchPost.js
+++ routes/post/matchPost.js
@@ -9,7 +9,12 @@ var Response = require('./response/postResponse');
 router.post('/',[checkIfMatched, matchpost]);
 
 function checkIfMatched(req,res,next){
-    Post.findOne({status: {$in:['match','read']}, match_to: req.body.user_id})
+    var dateRep = getDateRep()+1;
+    var weekTimeDayRep = dateRep-8;
+    Post.findOne({status: {$in:['match','read']}, 
+        match_to: req.body.user_id,
+        date_rep:{$gte :weekTimeDayRep , $lte : dateRep}
+                })
         .exec((err,doc)=>{
             if(err) throw err;
             if(doc){
@@ -40,6 +45,12 @@ function matchpost(req, res) {
                 res.status(200).send(resp);
             }
         });
+}
+
+function getDateRep(){
+    var serverDate = new Date();
+    var dateRep = serverDate.getDate+(serverDate.getMonth+1)*100+serverDate.getFullYear*10000;
+    return dateRep;
 }
 
 module.exports = router;
